26323bda9536389c95723c41c9f8f5e694c821f2
keithp <kapgoals@gmail.com>
2013-07-02 01:53:22 -0400

    added subtraction

--- keither.py
+++ keither.py
@@ -4,6 +4,13 @@ running_total = 0
 
 for x in list:
 	running_total = x + running_total
+	print running_total
+print running_total
+
+
+others = [4,2,2]
+for x in others:
+	running_total = running_total - x
 print running_total
 
 #ENDFILE
